10284d5d819846ead3aba8b6e7b31030f9c23f6c
Edoardo <ercolino.edoardo@gmail.com>
2021-07-20 14:47:30 +0200

    Scaffolding

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// 1. chiedi all’utente il cognome
+var domanda = prompt('Quale è il tuo cognome');
+
+// 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+var lista_cognomi = [Bianchi, Neri, Rossi, Verdi, Gialli];
+
+// 3. stampa la lista ordinata alfabeticamente
+consol.log()
+
+// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
