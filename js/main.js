66af26c8a9eccaa39b88857466fd5f48a513f8e5
Edoardo <ercolino.edoardo@gmail.com>
2021-07-20 15:15:47 +0200

    Inserimento cognome + ordine lista

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 // 1. chiedi all’utente il cognome
-var domanda = prompt('Quale è il tuo cognome');
+var cognome = prompt('Quale è il tuo cognome');
 
 // 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
-var lista_cognomi = [Bianchi, Neri, Rossi, Verdi, Gialli];
+var lista_cognomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
+lista_cognomi.push(cognome);
+console.log('la lista è composta da: ' + lista_cognomi);
 
-// 3. stampa la lista ordinata alfabeticamente
-consol.log()
+// 3a. stampa la lista ordinata alfabeticamente dalla A alla Z
+console.log('Lista dalla A alla Z: ' + lista_cognomi.sort());
+// 3b. stampa la lista ordinata alfabeticamente dalla Z alla A 
+console.log('Lista dalla Z alla A: ' + lista_cognomi.reverse());
+
 
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
